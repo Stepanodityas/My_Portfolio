7717b8a9b7ff3abede89df51611aa6ab2f607808
Stepanodityas <vanodityas13@gmail.com>
2026-08-06 07:28:37 +0700

    new stuff

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <main>
       {/* Header */}
 
-      <header className="sticky top-0 z-10 items-center bg-[#778873] px-8 py-4 w-full">
+      <header className="sticky top-0 z-10 items-center bg-[#111844] px-8 py-4 w-full h-full">
         <div className="flex justify-between font-sans text-3xl font-bold tracking-wide">
           <span className="hover:text-amber-400 transition-colors duration-200">
             Stevano
@@ -25,17 +25,17 @@ export default function Home() {
 
       {/* Hero Section */}
 
-      <div className="flex md:flex-row flex-col justify-between px-8 py-28 bg-[#F7F4ED]">
-        <div className="text-gray-700 font-sans font-bold text-4xl py-1.5">
-          Hi,{" "}
-          <span className="hover:text-[#41b5ef] transition-colors duration-200">
+      <section className="flex md:flex-row flex-col justify-between px-8 py-28 bg-[#fff3df]">
+        <div className="text-gray-700 font-sans font-bold text-6xl py-1.5">
+          <span className="text-blue-950">Hi, </span>
+          <span className="text-blue-950 hover:text-[#41b5ef] transition-colors duration-200">
             Welcome
           </span>
           <div className="font-light text-[20px] py-3">
             Im Stevano Dityas Nugraha
           </div>
           <div className="flex justify-between items-start text-left">
-            <h1 className="flex font-light text-[19px]">
+            <h1 className="flex font-light text-[16px]  ">
               <p>
                 A{" "}
                 <span className="font-bold hover:text-blue-600 transition-colors duration-200">
@@ -52,33 +52,16 @@ export default function Home() {
             </h1>
           </div>
         </div>
-      </div>
-
-      {/* Projects & Container */}
-
-      <div className="w-full px-15 py-7 bg-[#f4ecda]">
-        <div className="flex justify-between text-gray-700 font-bold font-sans text-2xl drop-shadow-2xl">
-          <h2 className="text-2xl text-gray-700 font-bold font-sans">
-            My said Projects
-          </h2>
-          <button className="text-[16px] rounded-lg text-white bg-mauve-700 hover:text-cyan-300 transition-all hover:lg:shadow-xl duration-200 px-2 hover:-translate-y-1.5 ">
-            Like button
-          </button>
+        <div className="flex justify-between px-8 py-28 ">
+          <h1>About Me</h1>
+          <p>Stevano Dityas Nugraha</p>
         </div>
-      </div>
+      </section>
 
-      <div className="px-40 py-10 bg-[#f4ecda] grid md:grid-cols-2  grid-cols-2">
-        <div className="bg-white rounded-lg drop-shadow-lg hover:-translate-y-4 transition-all duration-200">
-          <Image
-            className="rounded-lg w-full"
-            src="/image/coding-man.jpg"
-            alt="codingMan.jpg"
-            width={350}
-            height={350}
-          />
-          <div></div>
-        </div>
+      <div>
+        <div></div>
       </div>
+      {/* */}
     </main>
   );
 }
